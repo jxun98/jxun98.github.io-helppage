54e34099af266b264171e126ef5b7ab3d523e56b
ZoeGaouette <zoe.gaouette@gmail.com>
2020-07-22 13:15:27 -0700

    fixed css issue and changed pointer on hover

--- js/answerJSON.js
+++ js/answerJSON.js
@@ -1,12 +1,12 @@
 var answerJSON =
     {
         "draw": "<div class=\"answer-step\"> " +
-                    "<h2>Select your brush style.<\/h2> " +
+                    "<h2>Select your brush style.</h2> " +
                         "<p>Select your preferred brush from the tools menu located on the right side of the application.<\/p> " +
                         "<img src=\"./assets/brush-selection.JPG\" alt=\"brush-selection\"/> " +
                  "<\/div> " +
-                  "<div class=\"answer-step\"  " +
-                      "<h2>Draw to your heart's content!<\/h2> " +
+                  "<div class=\"answer-step\">  " +
+                      "<h2>Draw to your heart's content!</h2> " +
                            "<p>Hover your cursor over the canvas to place your brush and press down to create your masterpieces!<\/p>" +
                            "<img src=\"./assets/brush.gif\" alt=\"brush\"/> " +
                            "<p>We currently support four different brush types.<\/p> " +
